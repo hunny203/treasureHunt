b6421b297d283471e6c58d54bb7ce3fc643124fb
hunny203 <77975319+hunny203@users.noreply.github.com>
2021-07-01 19:07:25 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "DELHI";
+const accessCode2 = "RAJASTHAN";
+const accessCode3 = "KASHMIR";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("E L D I H", 100,50)
+    fill("lightblue")
+    text("Hint: The Capital  Of India !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("H A T N R J A A", 720,50)
+    fill("lightblue")
+    text("Hint: Largest State Of India !!", 720,70)
+
+    fill("darkblue")
+    textSize(15)
+    text("R I A S K H M", 500,400)
+    fill("darkblue")
+    text("Hint: Paradise On Earth !!", 500,420)
+
+}
---
+++ js/System.js
@@ -0,0 +1,14 @@
+class System{
+
+    constructor(){}
+
+    // Add code to authenticate the given code and the access codes.
+authenticate(actualCode,enterCode){
+    if(actualCode === enterCode)
+    return true
+    else
+    return false
+}
+    
+
+}
